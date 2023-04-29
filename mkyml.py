8beede80c387690b71302484877989b81a57a1e1
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-04-29 20:23:26 +0800

    Update mkyml.py
    ddd

--- mkyml.py
+++ mkyml.py
@@ -1,6 +1,7 @@
 import yaml
 import os
-aproject = {'tg_bot_token': os.get('TG_BOT_TOKEN'),'tg_chat_id': os.get('TG_CHAT_ID')}
+myyml = {'tg_bot_token': os.getenv('TG_BOT_TOKEN'),'tg_chat_id': os.getenv('TG_CHAT_ID'),'use_proxy':0,'use_proxy_dmm':0,'proxy_addr':'','tg_api_id':'','tg_api_hash':'','use_pikpak':0,'use_cache':0,'redis_host':'','redis_port':''}
 with open("/root/.tg_jav_bot/config.yaml", 'w') as f:
-  f.write(yaml.dump(aproject))
+    f.write(yaml.dump(myyml))
+
 
